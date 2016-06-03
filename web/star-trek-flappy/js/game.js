00e9234c397c54866d712b19ba2422bb3e79ebba
eric.chiu <wf870125@gmail.com>
2016-06-03 12:32:22 +0800

    fix timer event pause and resume issue

--- web/star-trek-flappy/js/game.js
+++ web/star-trek-flappy/js/game.js
@@ -29,15 +29,12 @@
     var score = 0;
     var startTime;
 
-
     // game objects
     var bgImg1, bgImg2;
     var shuttle;
     var rocks = [];
     var explosions = [];
     var MAX_VELOCITY = 200;
-    var rockUpTimerEvent;
-    var rockDownTimerEvent;
 
     // keys
     var spaceKey;
@@ -93,11 +90,6 @@
         shuttle.body.addCircle(shuttle.height * 0.5);
         shuttle.body.onBeginContact.add(onShuttleCrashed, this);
 
-        // rockUpTimerEvent = game.time.events.loop(game.rnd.integerInRange(2000, 5000), createRockUp, this);
-        // rockDownTimerEvent = game.time.events.loop(game.rnd.integerInRange(2000, 5000), createRockDown, this);
-        // rockUpTimerEvent.loop = false;
-        // rockDownTimerEvent.loop = false;
-
         // register key events
         spaceKey = game.input.keyboard.addKey(32);
 
@@ -132,7 +124,8 @@
             }
         } else {
             connectTxt.visible = true;
-            if (gameStart) {
+            game.world.bringToTop(connectTxt);
+            if (gameStart && !gamePaused) {
                 pauseGame();
             }
             return;
@@ -157,12 +150,8 @@
             // start creating rocks
             if (first) {
                 first = false;
-                // game.time.events.add(Phaser.Timer.SECOND * Math.random()*3, createRockUp, this);
-                // game.time.events.add(Phaser.Timer.SECOND * Math.random()*3, createRockDown, this);
-                rockUpTimerEvent = game.time.events.loop(game.rnd.integerInRange(2000, 5000), createRockUp, this);
-                rockDownTimerEvent = game.time.events.loop(game.rnd.integerInRange(2000, 5000), createRockDown, this);
-                // rockUpTimerEvent.loop = true;
-                // rockDownTimerEvent.loop = true;
+                game.time.events.add(Phaser.Timer.SECOND * Math.random()*3, createRockUp, this);
+                game.time.events.add(Phaser.Timer.SECOND * Math.random()*3, createRockDown, this);
             }
 
             // show score
@@ -203,18 +192,15 @@
     }
 
     function resumeGame() {
+        game.time.events.resume();
         gamePaused = false;
-        rockUpTimerEvent.loop = true;
-        rockDownTimerEvent.loop = true;
         var v = Math.floor(score/10) * 10 + 100;
         for (var i = 0; i < rocks.length; i++) rocks[i].body.velocity.x = (v > MAX_VELOCITY)? -MAX_VELOCITY : -v;
     }
 
     function pauseGame() {
+        game.time.events.pause();
         gamePaused = true;
-        rockUpTimerEvent.loop = false;
-        rockDownTimerEvent.loop = false;
-        game.world.bringToTop(connectTxt);
         for (var i = 0; i < rocks.length; i++) rocks[i].body.velocity.x = 0;
     }
 
@@ -297,9 +283,9 @@
 
         rocks.push(rock);
 
-        // if (gameStart) {
-        //     game.time.events.add(Phaser.Timer.SECOND * (Math.random()*4+1.5), createRockUp, this);
-        // }
+        if (gameStart) {
+            game.time.events.add(Phaser.Timer.SECOND * (Math.random()*4+1.5), createRockUp, this);
+        }
     }
 
     function createRockDown() {
@@ -333,9 +319,9 @@
 
         rocks.push(rock);
 
-        // if (gameStart) {
-        //     game.time.events.add(Phaser.Timer.SECOND * (Math.random()*4+1.5), createRockDown, this);
-        // }
+        if (gameStart) {
+            game.time.events.add(Phaser.Timer.SECOND * (Math.random()*4+1.5), createRockDown, this);
+        }
     }
 
     function moveShuttle(intensity) {
